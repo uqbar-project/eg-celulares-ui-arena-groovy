0bb0b5819fd8217950583af433751f6ebac081ed
Fernando Dodino <fernando.dodino@gmail.com>
2013-08-16 21:22:30 +0000

    Se crean test unitarios

---
+++ src/test/groovy/ar/edu/celulares/applicationModel/TestBuscadorCelular.groovy
@@ -0,0 +1,45 @@
+package ar.edu.celulares.applicationModel
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.uqbar.commons.utils.ApplicationContext
+
+import ar.edu.celulares.domain.Celular
+import ar.edu.celulares.domain.Modelo
+import ar.edu.celulares.home.HomeCelulares
+import ar.edu.celulares.home.HomeModelos
+
+class TestBuscadorCelular {
+
+	BuscadorCelular searcher
+	BuscadorCelular buscadorFallido
+	
+	@Before
+	def void init() {
+		searcher = new BuscadorCelular(nombre: "Do", numero: null)
+		buscadorFallido = new BuscadorCelular(nombre: "XXXX", numero: null)
+		ApplicationContext.instance.configureSingleton(Modelo.class, HomeModelos.instance)
+		ApplicationContext.instance.configureSingleton(Celular.class, HomeCelulares.instance)
+	}
+	
+	@Test
+	def void buscarSinResultados() {
+		buscadorFallido.search()
+		Assert.assertEquals(0, buscadorFallido.resultados.size())
+	}
+	
+	@Test
+	def void buscarDodinos() {
+		searcher.search()
+		Assert.assertEquals(2, searcher.resultados.size())
+	}
+
+	@Test
+	def void buscarDodinosConNumeroErroneo() {
+		searcher.numero = 17715274
+		searcher.search()
+		Assert.assertEquals(0, searcher.resultados.size())
+	}
+
+}
